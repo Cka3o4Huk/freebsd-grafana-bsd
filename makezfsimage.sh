c625d2e1305a1a8ba6764bb196353ec2d19f6213
Michael Zhilin <zhilin@netcracker.com>
2015-12-24 14:14:51 +0300

    (improvement) add makezfsimage.sh for dev testing

--- makezfsimage.sh
+++ makezfsimage.sh
@@ -4,6 +4,7 @@ IMAGENAME=grafana.img
 SITE='ftp.freebsd.org/pub/FreeBSD/snapshots/i386/i386/10.2-STABLE'
 DISTFILES=('base.txz' 'kernel.txz' 'doc.txz' 'ports.txz')
 
+echo "### Fetching files..."
 for DISTFILE in ${DISTFILES[@]}
 do
 	if [ ! -f $DISTFILE ]
@@ -14,12 +15,12 @@ done
 
 if [ -f $IMAGENAME ]
 then
-	echo "File exists"
 	FULLNAME=`readlink -f $IMAGENAME`
 	MD_UNIT=`mdconfig -l -v | grep $FULLNAME | cut -f1`
 	mdconfig -d -u $MD_UNIT
 fi
 
+echo "### Creating and mounting images..."
 rm -rf $IMAGENAME
 truncate -s 24G $IMAGENAME
 MD_UNIT=`mdconfig -a -f $IMAGENAME`
@@ -27,6 +28,7 @@ MD_UNIT=`mdconfig -a -f $IMAGENAME`
 echo "export ZFSBOOT_DISKS=$MD_UNIT" > installscript
 echo "BSDINSTALL_DISTSITE=ftp://$SITE" >> installscript
 cat bsdinstall.script >> installscript
-#bsdinstall script installscript
+echo "### Installing..."
+bsdinstall script installscript
 zpool export zroot
 mdconfig -d -u $MD_UNIT
