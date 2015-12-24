05cfb73a5070c37d7306377a7781a7a498d54d31
Michael Zhilin <zhilin@netcracker.com>
2015-12-24 14:54:03 +0300

    (improvement) add makezfsimage.sh for dev testing

--- makezfsimage.sh
+++ makezfsimage.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+CURRDIR=`pwd`
 IMAGENAME=grafana.img
 SITE='ftp.freebsd.org/pub/FreeBSD/snapshots/i386/i386/10.2-STABLE'
 DISTFILES=('base.txz' 'kernel.txz' 'doc.txz' 'ports.txz')
@@ -22,13 +23,14 @@ fi
 
 echo "### Creating and mounting images..."
 rm -rf $IMAGENAME
-truncate -s 24G $IMAGENAME
+truncate -s 24G $IMAGENsAME
 MD_UNIT=`mdconfig -a -f $IMAGENAME`
 
 echo "export ZFSBOOT_DISKS=$MD_UNIT" > installscript
 echo "BSDINSTALL_DISTSITE=ftp://$SITE" >> installscript
+echo "BSDINSTALL_DISTDIR=$CURRDIR" >> installscript
 cat bsdinstall.script >> installscript
 echo "### Installing..."
-bsdinstall script installscript
+bsdinstall script installscript < /dev/null 
 zpool export zroot
 mdconfig -d -u $MD_UNIT
