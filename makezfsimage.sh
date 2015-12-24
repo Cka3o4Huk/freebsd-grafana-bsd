75e41e7b22af03423fa13695a549ae3658c5df0c
Michael Zhilin <zhilin@netcracker.com>
2015-12-24 13:53:32 +0300

    (improvement) add makezfsimage.sh for dev testing

---
+++ makezfsimage.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DISTFILES=['base.txz', 'kernel.txz', 'doc.txz', 'ports.txz']
+
+for DISTFILE in DISTFILES
+do
+	if [ ! -f $DISTFILE ]
+	then
+		wget http://ftp.freebsd.org/pub/FreeBSD/snapshots/i386/i386/10.2-STABLE/$DISTFILE
+	fi
+done
+
+truncate -s 24G grafana.img
+MD_UNIT=`mdconfig -a -f grafana.img`
+
+echo "export ZFSBOOT_DISKS=$MD_UNIT" > installscript
+cat bsdinstall.script >> installscript
+bsdinstall script installscript
